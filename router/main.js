9801bf47cb6bc2c71e08b137e80cd83753846d04
ILikeTteokBoKki <38879025+MinByeongChan@users.noreply.github.com>
2021-02-03 10:38:18 +0900

    Session Test

--- router/main.js
+++ router/main.js
@@ -1,8 +1,12 @@
 module.exports = function (app, fs) {
   app.get("/", function (req, res) {
+    var sess = req.session;
+
     res.render("index", {
       title: "MY HOMEPAGE",
       length: 5,
+      name: sess.name,
+      username: sess.username,
     });
   });
 
@@ -82,5 +86,48 @@ module.exports = function (app, fs) {
         }
       );
     });
+  });
+
+  app.get("/login/:username/:password", (req, res) => {
+    var sess;
+    sess = req.session;
+
+    fs.readFile(__dirname + "/../data/user.json", "utf8", (err, data) => {
+      var users = JSON.parse(data);
+      var username = req.params.username;
+      var password = req.params.password;
+      var result = {};
+
+      if (!users[username]) {
+        result["success"] = 0;
+        result["error"] = "not found";
+        res.json(result);
+        return;
+      }
+
+      if (users[username]["password"] == password) {
+        result["success"] = 1;
+        sess.username = username;
+        sess.name = users[username]["name"];
+
+        console.log(sess);
+        res.json(result);
+      } else {
+        result["success"] = 0;
+        result["error"] = "incorrect";
+        res.json(result);
+      }
+    });
+  });
+
+  app.get("/logout", (req, res) => {
+    var sess = req.session;
+
+    if (sess.username) {
+      req.session.destroy((err) => {
+        if (err) console.log(err);
+        else res.redirect("/");
+      });
+    }
   });
 };
